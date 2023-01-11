136377cd2af312726eaf2e1b79954e105c0e53ca
yjw4110 <yjw4110@gmail.com>
2023-01-11 17:04:30 +0900

    newupdate230111

--- 02_OUTBACK_steackhouse/js/script.js
+++ 02_OUTBACK_steackhouse/js/script.js
@@ -50,6 +50,7 @@ $('.swiper-button-next').css({cursor:'url(./outbackimage/meat2.png),auto'})
 $('.swiper-button-prev').css({cursor:'url(./outbackimage/meat2.png),auto'})
 $('.swiper-pagination-bullet').css({cursor:'url(./outbackimage/meat2.png),auto'})
 $('.footer3 .footer_wrap .main_footer1 .container .footer-left-menu .family-site select').css({cursor:'url(./outbackimage/meat2.png),auto'})
+$('.footer3 .footer_wrap .sns-menu img').css({cursor:'url(./outbackimage/meat2.png),auto'})
 
 /* topbutton */
 $(document).ready(function(){
